80fdc227f09c6aed0cc14e40ca24dbae2c7ee32f
Michael Saxer <137685346+Saxermi@users.noreply.github.com>
2025-03-12 11:33:17 +0100

    Update model.py

--- CVDL_Bsc-main/labs/01_image_classification/model.py
+++ CVDL_Bsc-main/labs/01_image_classification/model.py
@@ -38,73 +38,38 @@ class NeuralNetwork(nn.Module):
 
 
 class CNN_classifier(nn.Module):
-    def __init__(self, n_classes,dropout = 0.2):
+    def __init__(self, n_classes, dropout=0.2):
         super(CNN_classifier, self).__init__()
-        # in_channels = 3 because 3 rgb channels, 16 out = umbers of feature maps extracted, stride =1, padding = 1 adds 1 pixel padding, for zero padding
-        # we could later introduce 1 paddinfg in the first layer to FORCE features activation in the first  layer and prevent
-        # neurons from dying paddin (optional)
-        # feature extractio n
-        self.conv1 = nn.Conv2d(
-            in_channels=3, out_channels=16, kernel_size=3, padding=1, stride=1
-        )
-        # output size = (input+2*padding-kernel size/stride) +1
-        # check if 1 pixel is sufficent
-        self.conv2 = nn.Conv2d(
-            in_channels=16, out_channels=16, kernel_size=5, stride=1, padding=2
-        )  # set padding to 2 to maintain 22x22 size
-        # we enforce the same output of 11 x 11 here by using adaprtive avg pooling
-        self.adaptiveavgpooling = nn.AdaptiveAvgPool2d((25, 25))
-        # self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        # assuming 16 feature maps and 22*22 inital image size (reduced half trough pooling)
-        # classification
-        self.neurallayer = nn.Linear(16 * 25 * 25, 1024)
-        self.neurallayer2 = nn.Linear(1024, 1024)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=5, stride=1, padding=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)  # Max Pooling
 
-        self.leakyrelu = nn.LeakyReLU()  # leaky relu to prevent vanishing gradient
-        self.neurallayer3 = nn.Linear(1024, n_classes)
+        self.adaptiveavgpooling = nn.AdaptiveAvgPool2d((10, 10))
+        self.neurallayer = nn.Linear(16 * 10 * 10, 512)
+        self.neurallayer2 = nn.Linear(512, 256)
+        self.neurallayer3 = nn.Linear(256, n_classes)
+        self.leakyrelu = nn.LeakyReLU()
         self.bnorm = nn.BatchNorm2d(16)
-        self.dropout = nn.Dropout(dropout)  # Prevenfdt overdfitting
-
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         network = self.conv1(x)
-        network = self.leakyrelu(
-            network
-        )  # applying a relu here else its a linear transformation
+        network = self.bnorm(network)
+        network = self.leakyrelu(network)
+        network = self.pool(network)
         network = self.conv2(network)
         network = self.bnorm(network)
-
         network = self.leakyrelu(network)
         network = self.adaptiveavgpooling(network)
         network = network.view(network.size(0), -1)
         network = self.neurallayer(network)
         network = self.leakyrelu(network)
+        network = self.dropout(network)
+
         network = self.neurallayer2(network)
         network = self.leakyrelu(network)
-        network = self.neurallayer2(network)
-        network = self.leakyrelu(network) #//maybe later add a dropout layer
         network = self.dropout(network)
         return self.neurallayer3(network)
-<<<<<<< HEAD
-class PretrainedClassifier(nn.Module):
-    def __init__(self, num_classes=37, pretrained=True, dropout=0.3):
-        super(PretrainedClassifier, self).__init__()
-        
-        # Load a pretrained ResNet18 model
-        self.base_model = models.resnet18(pretrained=pretrained)
-        
-        # Modify the final fully connected layer to match the number of classes
-        in_features = self.base_model.fc.in_features
-        self.base_model.fc = nn.Sequential(
-            nn.Linear(in_features, 512),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(512, num_classes)
-        )
-    
-    def forward(self, x):
-        return self.base_model(x)
-=======
 #Trainable parameters in FFNN: 104937509
-#Trainable parameters in CNN: 11335445
->>>>>>> ad813dbc454771d089fffab550144744d39ed56c
+#Trainable parameters in CNN: 967445
+
